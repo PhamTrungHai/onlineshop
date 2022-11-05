78ec38a3cf496effa5a456503a3535892000fca5
HaiPhham <haiphamtrung62@gmail.com>
2022-11-05 18:12:26 +0700

    ShopOnline

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <nav>
+          <header className="App-header">
+        <a href='/'> Laptopshop</a>
+          </header>
+      </nav>
+      <main>
+        list
+      </main>
+    </div>
+  );
+}
+
+export default App;
