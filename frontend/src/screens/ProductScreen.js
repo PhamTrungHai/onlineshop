d79f985da1641a93eb1c1dbf5de7f2ad522fa1a7
HaiPhham <haiphamtrung62@gmail.com>
2022-11-11 13:04:49 +0700

    11/11/22 1

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -50,10 +50,18 @@ function ProducScreen() {
   }, [slug]);
 
   const { state, dispatch: ctxDispatch } = useContext(Store);
-  const addToCartHandle = () => {
+  const { cart } = state;
+  const addToCartHandle = async () => {
+    const existItem = cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      window.alert('Sản phẩm đã hết hàng');
+      return;
+    }
     ctxDispatch({
       type: 'CART_ADD_ITEM',
-      payload: { ...product, quantity: 1 },
+      payload: { ...product, quantity },
     });
   };
 
