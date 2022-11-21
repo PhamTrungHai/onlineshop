f88c94025b7b4517a8a083d0eb2cc9f4c9678772
HaiPhham <haiphamtrung62@gmail.com>
2022-11-21 18:21:33 +0700

    Setup database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,22 @@
 import express from 'express';
 import data from './data.js';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import sql from 'mssql';
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to db');
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
 const app = express();
+
 //
 app.get('/api/products', (req, res) => {
   res.send(data.products);
