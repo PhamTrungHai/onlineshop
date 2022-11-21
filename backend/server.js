4af138faecf9616db4a414a0c93d3eebd91d92bf
HaiPhham <haiphamtrung62@gmail.com>
2022-11-21 19:44:37 +0700

    userRoutes created util created

--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,10 @@ import data from './data.js';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import sql from 'mssql';
+
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -18,9 +20,17 @@ mongoose
   });
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use('/api/seed', seedRouter);
-//
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
