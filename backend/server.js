677d727c0736b388dc8c08397f54604aa080d3d2
HaiPhham <haiphamtrung62@gmail.com>
2022-12-07 16:02:00 +0700

    4:01

--- backend/server.js
+++ backend/server.js
@@ -25,12 +25,9 @@ const app = express();
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'Access-Control-Allow-Headers',
-    'Content-Type',
-    'Authorization'
-  );
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Headers', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'Authorization');
   next();
 });
 
