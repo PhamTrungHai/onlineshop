cd6a4e97029ca002dc9ccf5a451a447abe778c28
HaiPhham <haiphamtrung62@gmail.com>
2022-12-07 15:37:16 +0700

    Access-Control-Allow-Headers

--- backend/server.js
+++ backend/server.js
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE');
   res.setHeader(
     'Access-Control-Allow-Methods',
+    'Access-Control-Allow-Headers',
     'Content-Type',
     'Authorization'
   );
